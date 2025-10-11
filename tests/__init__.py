585c0161ed804dbbe65337c70d03fef707119d45
Philipp Benner <philipp@zeroprobability.org>
2025-10-11 16:19:43 +0200

    added missing __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+"""Test utilities package for cuequivariance adapter."""
+
+import pytest
+
+pytestmark = [
+    pytest.mark.filterwarnings(
+        "ignore::DeprecationWarning:haiku._src.transform"
+    ),
+    pytest.mark.filterwarnings(
+        "ignore::DeprecationWarning:jax.lib.xla_extension"
+    ),
+]
